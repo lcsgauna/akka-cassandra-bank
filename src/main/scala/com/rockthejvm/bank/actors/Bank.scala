90761ad422c862a89ddc82a78a35ea0013fd3054
Lucas Gauna <lucasgsamudio@gmail.com>
2022-09-11 23:00:25 -0400

    Üpdate bank

--- src/main/scala/com/rockthejvm/bank/actors/Bank.scala
+++ src/main/scala/com/rockthejvm/bank/actors/Bank.scala
@@ -1,9 +1,12 @@
 package com.rockthejvm.bank.actors
 
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import akka.actor.typed.{ActorRef, Behavior}
-import akka.persistence.typed.scaladsl.Effect
+import akka.persistence.typed.PersistenceId
+import akka.persistence.typed.scaladsl.{Effect, EventSourcedBehavior}
+import java.util.UUID
 
-class Bank {
+object Bank {
   //commands = messages
   import PersistentBankAccount.Command._
   import PersistentBankAccount.Command
@@ -16,14 +19,29 @@ class Bank {
   case class State(accounts:Map[String,ActorRef[Command]])
 
   // command handler
-  val commandHandler : (State,Command) => Effect[Event,State] = ???
+  def commandHandler(context:ActorContext[Command]) : (State,Command) => Effect[Event,State] = (state, command) =>
+    command match {
+      case createCommand @ CreateBankAccount(_,_,_,_) =>
+        val id = UUID.randomUUID().toString
+        val newBankAccount = context.spawn(PersistentBankAccount(id),id)
+        Effect
+        .persist(BankAccountCreated(id))
+        .thenReply(newBankAccount)(_=>createCommand)
+      case updateCmd @ UpdateBalance(id, currency, amount, replyTo) =>
+        state.accounts.get(id) match {
+          case Some(account) =>
+            Effect.reply(account)()
+        }
+    }
   // event handler
   val eventHandler : (State,Event) => State = ???
   // behavior
-  def apply() : Behavior[Command] = ???
-
-
-
-
-
+  def apply() : Behavior[Command] = Behaviors.setup { context =>
+    EventSourcedBehavior[Command, Event, State](
+      persistenceId = PersistenceId.ofUniqueId("bank"),
+      emptyState = State(Map()),
+      commandHandler = commandHandler(context),
+      eventHandler = eventHandler
+    )
+  }
 }
